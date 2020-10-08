561cf6d8488bd23cbaf9308b78940d0f08f53f85
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-08 22:23:04 +0700

    Memperbaiki Css dan JS link di View Header dan Footer

--- app/views/Templates/header.php
+++ app/views/Templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["judul"]; ?></title>
 
-    <link rel="stylesheet" href="<?= BASEURL ?>css/paper.css">
+    <link rel="stylesheet" href="<?= BASEURL ?>assets/css/paper.css">
 </head>
 
 <body>
